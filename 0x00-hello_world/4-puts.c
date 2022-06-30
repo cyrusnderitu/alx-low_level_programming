c7310ccb2215ce0bbdf6300033bc8a162e3345a4
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-06-30 15:12:59 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main (void) { 
-char str1[] = "Programming is like building a multilingual puzzle\n";
- puts(str1);  
- return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char str1[] = "Programming is like building a multilingual puzzle\n";
+    puts(str1);
+    return (0);
 }
