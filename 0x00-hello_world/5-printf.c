066d74d6b8bc395033e050e5aac34ec72808933c
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-06-30 15:49:47 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,6 @@
  */
 int main(void) 
 {  
-char str1[] = "\"Programming is like building a multilingual puzzle";
-printf("%c", str1)
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
