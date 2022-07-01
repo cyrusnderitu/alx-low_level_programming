bcdeddc356736609a49a11dc7a58adcc7823d654
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-01 18:37:46 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n, last_digit;
