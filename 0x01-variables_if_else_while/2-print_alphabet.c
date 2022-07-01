cdd7f6989e7e2e3b21e9050f563a9e32314431a4
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-01 19:41:27 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  */
 int main (void)
 {
-for(int x = 'A'; x <= 'Z';  x++)
+int x ;
+for(x = 'A'; x <= 'Z';  x++)
 {
-int lower_x = tolower(x);
-putchar(lower_x\n)
+putchar(tolower(x));
 }
 return (0);
 }
