9420fa573ae027ff1a7fa0c8f10ca4da8e37ffb3
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-01 20:45:09 -0700

    C program for Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */  
+int main (void)
+{
+int i = 0;
+do
+{
+  printf("%d",  i);
+  i++;
+}
+ while(i< 10);
+ putchar("\n");
+ return (0);
+}
