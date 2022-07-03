e873d02fe007192c73ec4afb6c608845189fcd8f
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-03 10:27:27 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints all possible combinations of single-digit 
+ * numbers.
+ * only using putchar and without char variables.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+{
+putchar((num % 10) + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
