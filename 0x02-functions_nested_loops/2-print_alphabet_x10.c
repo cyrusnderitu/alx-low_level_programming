26af802b28ffd9752394186844a07ea3a846c0b2
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-05 16:18:57 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,29 +1,19 @@
 #include "main.h"
 
-
-
 /**
-
  * print_alphabet - Print lowercase
-
  * Return: Always 0 (Success)
-
  */
-
-void print_alphabet(void)
-  
+void print_alphabet_x10(void); 
 {
-  
-  char c;
-  
-  for (c = 'a'; c <= 'z'; c++)
-    
-    {
-      
-      _putchar(c);
-      
-    }
-  
-  _putchar('\n');
-  
+int count = 0;
+char c;
+while(count <= 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+ _putchar(c); 
+}
+_putchar('\n');
+}
 }
