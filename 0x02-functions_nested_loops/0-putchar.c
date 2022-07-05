80aab4e9216b210b23840f5b1c22695f834a3c15
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-05 03:53:16 -0700

    Initial Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - A progam that prints
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchat('h');
+  putchar('a');
+  putchar('r');
+return (0);
+}
