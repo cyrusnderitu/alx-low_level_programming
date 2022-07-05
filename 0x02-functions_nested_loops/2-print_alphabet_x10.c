119385ea8b783504b65828b111e8413fc194d621
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-05 06:15:05 -0700

    C program for 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_alphabet - Print lowercase
+
+ * Return: Always 0 (Success)
+
+ */
+
+void print_alphabet(void)
+  
+{
+  
+  char c;
+  
+  for (c = 'a'; c <= 'z'; c++)
+    
+    {
+      
+      _putchar(c);
+      
+    }
+  
+  _putchar('\n');
+  
+}
