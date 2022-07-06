47874c5ecf0016ae60e3b417a4aac3858be1f624
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-06 05:28:14 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,10 +6,10 @@
  *
  * Return: The absolute value of the integer.
  */
-int _abs(int n) 
-{  
-if (n >= 0)    
-return (n);  
-else    
-return (-n); 
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
 }
