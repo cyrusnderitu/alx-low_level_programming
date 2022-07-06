a1c45f723037376097987cd4f032647daec6df3e
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-06 04:54:55 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,13 @@
 /**
  * _isalpha - checks for alphabetic characters
  * @c: The character to be checked
- * Return: 1 if character is a lowercase or uppercase letter, otherwise return 0
+ * Return: 1 if character is a lowercase or uppercase letter, 
+ * otherwise return 0
  */
 int _isalpha(int c)
 {
-  if((c >= 97 && c<=122) || (c>=65 && c<=90))
-    return(1);
-  else
-    return(0);
+if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return(1);
+else
+return(0);
 }
