454eb3750f55b0204d276257013524fdd832a4c0
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-06 04:52:05 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,4 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-<<<<<<< HEAD
 int _isalpha(int c);
-=======
->>>>>>> 049337c937fe399ee53aac5ab3cff7a271845093
