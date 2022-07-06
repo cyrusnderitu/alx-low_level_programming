b03f894fbdbcc89ba4d7aa4de0602b78194b51b3
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-06 06:37:50 -0700

    C program for 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Print natural numbers from n to 98
+ * @n: n input to print to 98
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int
+ {
+   if (n >= 98)
+     
+     {
+       
+       while (n > 98)
+	 
+	 printf("%d, ", n--);
+       
+       printf("%d\n", n);
+       
+     }
+   
+
+   
+   else
+     
+     {
+       
+       while (n < 98)
+	 
+	 printf("%d, ", n++);
+       
+       printf("%d\n", n);
+       
+     }
+ }
