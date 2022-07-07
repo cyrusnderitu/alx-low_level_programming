452e776382e1a175747915dd489c232607f4f25e
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-07 09:24:20 -0700

    C program for I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  Print numbers excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+for(int c = '0'; c <= '9'; c++)
+{
+if(c != '2' && c != '4')
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+return 0;
+}
