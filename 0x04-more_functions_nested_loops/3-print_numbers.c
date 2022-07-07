18546c4dfa3386318f60580ff30c3e119daa505c
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-07 15:51:12 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,13 @@
 
 /**
  * print_numbers - _putchar digits from (0 - 9)
- * 
- * Return: print digits from 0 to 9
  */
 void print_numbers(void)
 {
-for(int c >= '0'; c <= '9'; c++)
+for (int c >= '0'; c <= '9'; c++)
 {
-_putchar((c % 10) + '0'));
+_putchar ((c % 10) + '0'));
 }
-_putchar('\n');
+_putchar ('\n');
 return (0);
 }
