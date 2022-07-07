ac35182b1fffc5b5b8f417862647cb2d767437d3
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-07 19:32:36 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_most_numbers(void)
 {
-for(int c = 0; c <= 9; c++)
+for (int c = 0; c <= 9; c++)
 {
-if(c != 2 && c != 4)
+if (c != 2 && c != 4)
 {
-_putchar((num % 10) + '0');
+_putchar (c);
 }
 }
-_putchar('\n');
+_putchar ('\n');
 return 0;
 }
