1972300363fe4da21936890e9625874bcedadd2c
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-07 05:25:16 -0700

    C program for The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - _putchar digits from (0 - 9)
+ * 
+ * Return: print digits from 0 to 9
+ */
+void print_numbers(void)
+{
+  for(int c >= '0'; c <= '9'; c++)
+{
+  _putchar(c);
+}
+  _putchar('\n')
+return (0);
+}
