4a9b5a7af7f9a050ef78d528406125a1638b8dd2
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-08 05:52:05 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,7 @@ if (n > 0)
 {
 for (len = 0; len < n; len++)
 {
-for (space = 0; space < len; space++)	    
+for (space = 0; space < len; space++)
 _putchar(' ');
 _putchar('\\');
 if (len == n - 1)
