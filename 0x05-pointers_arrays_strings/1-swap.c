8587e36ac7ed944a50bdcc32d54bd0321558b3da
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-11 18:59:54 -0700

    Cprogram for Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+  return (0);
+}
