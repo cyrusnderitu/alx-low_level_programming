6de34f87c56e4fd2794e27a65651b8759045f3c7
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-12 04:16:35 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,10 @@
 
 /**
  * reset_to_98 - updates the value it points to
- * Return: Always 0 (Success)
+ *@n: parameter change
+ * Return: (Success)
  */
 void reset_to_98(int *n)
 {
-int *r = &n;
-*r = 98;
-_putchar(n)
-return (0);
+*n = 98;
 }
