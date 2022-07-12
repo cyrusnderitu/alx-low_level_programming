4cd93395699021e5f88a4979e189642d9b75e077
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-11 19:21:32 -0700

    C program for A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * rev_string - Printing a reverse string
+ * @s: paramter to be used fro rev string
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+  return (0);
+}
