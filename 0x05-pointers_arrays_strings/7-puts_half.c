b3d8903cf6430356648a561b0b3d0a786df6b49e
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-12 06:31:13 -0700

    C program for Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @n: holds half of the string
+ * @str: String provided
+ */
+void puts_half(char *str)
+{
+  int len = 0, index, n;
+  while(str[len])
+    len++;
+  if((len % 2) == 0)
+    n = len / 2;
+  else
+    n = (len -1) / 2;
+  for(index = n; index <= len; index++)
+    _putchar(str[index]);
+  _putchar('\n')
+}
