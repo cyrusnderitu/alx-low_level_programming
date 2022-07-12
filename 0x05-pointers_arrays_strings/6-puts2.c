f3494f8d39cc406367545476f1e568d06d17f4bb
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-11 19:27:03 -0700

    C program for Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string
+ * @str: paramter for reference
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+  return (0);
+}
