aafe751cba694e423a04190b8dd2676e7d5ad2d7
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-14 07:33:41 -0700

    C program for strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strncat - Concatenates two strings
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+char **ptr;
+ptr = &dest;
+ptr = strcpy(dest, src)
+return (ptr)
+}
