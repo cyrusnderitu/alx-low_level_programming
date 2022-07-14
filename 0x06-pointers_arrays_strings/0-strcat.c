460b9c325abdd34fa9ba65cd004f2fdc3394a4c6
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-14 17:38:26 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,8 +11,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-char **ptr;
-ptr = &dest;
-ptr = strcat(dest, src)
-return (ptr)
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
