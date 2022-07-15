ceb2c69186459376167e98c1306a515497c6fb6d
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-15 05:33:48 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#inlude <string.h>
 
 /**
  * *_strncat - Concatenates two strings
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
- *
  * Return: A pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
