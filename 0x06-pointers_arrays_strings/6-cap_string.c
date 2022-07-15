f181755e3198ef20bcf54b64d3904a17b2bd4cec
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-15 14:07:32 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,6 @@
 /**
  * *cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
- *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -32,6 +31,5 @@ str[index] -= 32;
 index++;
 }
 }
-
 return (str);
 }
