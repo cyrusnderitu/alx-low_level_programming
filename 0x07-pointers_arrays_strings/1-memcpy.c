35ca5c4e8c9249d6032224dbd6c5980496a6ec30
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-18 22:38:12 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,8 +2,9 @@
 
 /**
  * *_memcpy - function that copies memory area
- * @dest file to be copied into
+ * @dest: file to be copied into
  * @src: file to copy from
+ * @n: the number of time copied in src
  * Return: The Copied into file i.e dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
