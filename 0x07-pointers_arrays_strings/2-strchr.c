8a149fa37f1d5cf37d48d1f96c4d713ad32e3e55
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-18 23:21:22 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,16 @@
  */
 char *_strchr(char *s, char c)
 {
-  int index = 0;
-  while(s[index++])
- {
+int index = 0;
+while(s[index++])
+{
 if(s[index] == c)
 {
-  return (s + index);
+return (s + index);
 }
- else
- {
-   return (Null);
- }
- }
+else
+{
+return (Null);
+}
+}
 }
