4e3b24bed3e1eaa172fc00fb36f03f0bd6d951a0
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-18 22:58:44 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 unsigned int index;
 for (index = 0; index < n; index++)
 {
-src[index] = dest[index];
+dest[index] = src[index];
 }
 return (dest);
 }
