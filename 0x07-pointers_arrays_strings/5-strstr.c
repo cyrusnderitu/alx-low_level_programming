4c2a7d5b1aaeb5e0cad0eb65ee0d355d4f998a26
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-18 19:57:16 -0700

    C program for strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: string to be scanned
+ * @needle: sub-string to be searched in haystack string
+ * Return:  pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+  int index = 0;
+  while (haystack[index++])
+    {
+      if (needle[index] == haystack[index])
+	{
+	  return (haystack + index);
+	}
+    }
+  return ('\0');
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ a function that copies memory area
 Write a function that locates a character in a string
 Write a function that gets the length of a prefix substring
 a function that searches a string for any of a set of bytes
+a function that locates a substring
