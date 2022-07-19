c5c098dc604b542d5706234eb2d9e16793f88ac9
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-18 19:34:54 -0700

    C program for strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - function that searches a string for any of a set of bytes
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ * Return: Pointer s that matches the the accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+  int index;
+  while (s[index++])
+    {
+      for (index = 0 ; accept[index]; index++)
+	{
+	  if (*s == accept[index])
+	    {
+	      return (s);
+	    }
+	}
+    }
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ a function that fills memory with a constant byte
 a function that copies memory area
 Write a function that locates a character in a string
 Write a function that gets the length of a prefix substring
+a function that searches a string for any of a set of bytes
