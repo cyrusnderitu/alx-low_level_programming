8fbb9267c969888d1128142ef081db34e7fde34f
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-19 15:59:00 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 int index;
 if (*needle == 0)
 return (haystack);
-while (haystack)
+while (*haystack)
 {
 index = 0;
 if (haystack[index] == needle[index])
@@ -21,7 +21,7 @@ if (needle[index + 1] == '\0')
 return (haystack);
 index++;
 }
-while (haystack[index] == needle[index])
+while (haystack[index] == needle[index]);
 }
 haystack++;
 }
