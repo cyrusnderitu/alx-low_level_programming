c8b2ad5373ed20b703b09ccb888c42adde4704e1
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-21 16:00:18 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,83 +1,42 @@
 #include "main.h"
 
-int is_divisible(int num, int div);
-
-int is_prime_number(int n);
-
-
-
+int prime_check(int digit, int div);
 /**
-
- * is_divisible - Checks if a number is divisible.
-
- * @num: The number to be checked.
-
+ * prime_check - Checks if a number is prime by divibility of 2.
+ * @digit: The number to be checked.
  * @div: The divisor.
-
- *
-
  * Return: If the number is divisible - 0.
-
- *         If the number is not divisible - 1.
-
  */
-
-int is_divisible(int num, int div)
-  
+int prime_check(int digit, int div)
 {
-  
-  if (num % div == 0)
-    
-    return (0);
-  
-
-  
-  if (div == num / 2)
-    
-    return (1);
-  
-
-  
-  return (is_divisible(num, div + 1));
-  
+if (digit % div == 0)
+{
+return (0);
 }
-
-
-
-/**
-
- * is_prime_number - Checks if a number is prime.
-
- * @n: The number to be checked.
-
- *
-
- * Return: If the integer is not prime - 0.
-
- *         If the number is prime - 1.
-
- */
-
-int is_prime_number(int n)
-  
+else
 {
-  
-  int div = 2;
-  
-
-  
-  if (n <= 1)
-    
-    return (0);
-  
-
-  
-  if (n >= 2 && n <= 3)
-    
-    return (1);
-  
-
-  
-  return (is_divisible(n, div));
-  
+return (prime_check(digit, div + 1));
+}
+}
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The integer to be checked.
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
+ */
+int is_prime_number(int n)  
+{  
+int div = 2;
+if (n <= 1)
+{
+return (0);
+}
+else if (n >= 2 && n <= 3)
+{
+return (1);
+}
+else
+{
+return (is_divisible(n, div));
+}
 }
