7fbe34320e42e9aef0772cd6b6013b2adf2c1968
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-21 00:27:22 -0700

    C Recusion for Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - caluculates the power of x raised to y
+ * @x: The integer to power
+ * @y: The power of the integer.
+ * Return: -1 if y is less 0, otherwise cal power.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1)
+}
+else
+return (x**y);
+}
