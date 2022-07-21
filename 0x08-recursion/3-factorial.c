4731a220beaf67993ba44e77248767c5e5d901a2
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-20 20:02:58 -0700

    C Recursions for You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - Gives the factorial of a given number.
+ * @n: number to find the factorial of.
+ * Return: 0 if n is greater than o and -1 if n is less.
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n = 0)
+{
+return (1);
+}
+else
+{
+int ans = n*factorial(n-1);
+return (ans);
+}
