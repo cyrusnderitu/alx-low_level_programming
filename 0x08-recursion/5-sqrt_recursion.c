02a22f0746edb1350670bd8e3de9e84cc071aff8
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-07-21 05:07:08 -0700

    C recursion for Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finding sqrt with recursion
+ * @n: The number to sqrt.
+ * Return: -1 if number is not natural number or
+ * the root if number is natural.
+ */
+int calc_sqrt(int n, int rt)
+{
+  if (rt * rt == num)
+    {
+      return (rt);
+    }
+  else
+    {
+      return (calc_sqrt(n, rt +1));
+    }
+}
+
+int _sqrt_recursion(int n)
+{
+  int root = 0;
+  if (n < 0)
+    {
+      return (-1);
+    }
+  else if (n == 1)
+    {
+      return (1);
+    }
+  else
+    {
+      return(calc_sqrt(n, root))
+}
