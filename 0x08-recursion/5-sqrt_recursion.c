0e72a73f1d1da2c7548884e45c8f24a6ea5c39a6
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2022-07-21 15:09:27 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,28 +8,27 @@
  */
 int calc_sqrt(int n, int rt)
 {
-  if (rt * rt == num)
-    {
-      return (rt);
-    }
-  else
-    {
-      return (calc_sqrt(n, rt +1));
-    }
+if (rt * rt == num)
+{
+return (rt);
 }
-
+else
+{
+return (calc_sqrt(n, rt +1));
+}
+}
 int _sqrt_recursion(int n)
 {
-  int root = 0;
-  if (n < 0)
-    {
-      return (-1);
-    }
-  else if (n == 1)
-    {
-      return (1);
-    }
-  else
-    {
-      return(calc_sqrt(n, root))
+int root = 0;
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 1)
+{
+return (1);
+}
+else
+{
+return(calc_sqrt(n, root))
 }
