18902b6a7a0903ab0a5947aad34b9c95a692d5b0
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-04-27 19:57:05 -0700

    Advanced tasks submission

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
+void __attribute__((constructor)) before(void);
 /**
- *  * before_main - function executed before main
- *   * Return: void
- *    */
-void before_main(void)
+ * before - execute before main
+ * Return: void
+ */
+void before(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
