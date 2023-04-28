2e3ca97c3e62e40f85c56da2ae33b29c2e7fb847
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-04-27 18:02:31 -0700

    Listing the number of elements existing in list_t linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * list_len - Counting the number of nodes available
+ * @h: the list of struct list_t
+ * Return: the number of elem in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+	while (h)
+	{
+		h = h -> next;
+		nodes++;
+	}
+return (nodes);
+}
