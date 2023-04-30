3a1b783b749bf43ed1c34bd8a63d695d78ff9666
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-30 06:38:24 +0300

    Betty coding styles correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,17 +4,18 @@
  * int_index - Function for integer comparrison
  * @array: array containing int for compairing
  * @size: size of array
- * @cmp: function pointer for compairing int
+ * @cmp: function pointer for compairing int.
+ * Return: Always success(0) else (-1)
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
-	for (i = 0; i < size ; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
+int i;
+if (array == NULL || cmp == NULL || size <= 0)
 	return (-1);
+for (i = 0; i < size ; i++)
+{
+	if (cmp(array[i]) != 0)
+		return (i);
+}
+return (-1);
 }
