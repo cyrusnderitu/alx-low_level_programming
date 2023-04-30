419c646fe26e4c3393482655ccafd2ae2033b25f
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-30 18:04:12 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,9 +7,9 @@
  */
 int main(int argc, char argv[])
 {
-	int num1, num2, result;
+	int num1, num2, result, i;
 	char *op;
-	char err = "Error";
+	char err[] = "Error";
 	if (argc > 4)
 		_putchar(err);
 		exit(98);
@@ -18,7 +18,8 @@ int main(int argc, char argv[])
 	num2 = atoi(argv[3]);
 	if ((op[0] == '/' || op[0] == '%') && num2 == 0)
 	{
-		_putchar(err);
+		while (err[i] != '0')
+		_putchar(err[i++]);
 		exit(100);
 	}
 	result = get_op_funct(op)(num1, num2);
