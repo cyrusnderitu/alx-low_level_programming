af4d0e29b66a8cb8d17bea6f2d76cb3fc19ebe8d
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-05-01 14:31:13 +0300

    Betty correction of lines at beginning

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t len = 0;
-    while (h)
-    {
-        _putchar(h->n + '0');
-        _putchar('\n');
-        h = h->next;
-        len++;
-    }
-    return (len);
+size_t len = 0;
+while (h)
+{
+_putchar(h->n + '0');
+_putchar('\n');
+h = h->next;
+len++;
+}
+return (len);
 }
