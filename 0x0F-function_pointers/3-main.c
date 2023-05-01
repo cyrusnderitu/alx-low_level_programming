7d75a793ec30ab38c56d50e9c9bdfcd64d356883
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-05-01 07:34:54 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,38 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - main function to run ops
- * @argc - count of argv
- * @argv - array of values passed on func call
- * Return: Always success(0) else (-1) or NULL
+ * main - return result of desired calculation from command line options
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: EXIT_FAILURE if unsuccessful or EXIT_SUCCESS if success.
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, result, i = 0;
-int (*op_func)(int, int);
-char *op;
-char err[] = "Error";
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-op = argv[2];
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-if ((op[0] == '/' || op[0] == '%') && num2 == 0)
-{
-printf("Error\n");
-exit(100);
-}
-op_func = get_op_func(op);
-result = op_func(num1, num2);
-_putchar(result + '0');
-_putchar('\n');
-exit(EXIT_SUCCESS);
+	int num1, num2, calc;
+	int (*op_func)(int, int);
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	op_func = get_op_func(operator);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	calc = op_func(num1, num2);
+	printf("%d\n", calc);
+	exit(EXIT_SUCCESS);
 }
