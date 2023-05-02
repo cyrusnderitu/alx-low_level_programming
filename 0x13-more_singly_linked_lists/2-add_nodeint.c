ad298db56014ca57235d2a071ac753d19565f9ab
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-05-02 10:13:39 -0700

    Adding elem at the start of listint_t

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adding a node at the beginning of list
+ * @head: noe to be added
+ * @n: value existing in the node.
+ * Return: the list of elem
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
