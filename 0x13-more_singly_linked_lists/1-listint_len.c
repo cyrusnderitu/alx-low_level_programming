063468245f089f8f3dac4b80131caa4db50cfc61
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-05-02 10:01:13 -0700

    Measuring num of elem in listint_t with test files

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - Func for num of elem in listint_t
+ * @h: head.
+ * Return: Number of elem in listint_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t elem = 0;
+	while (h)
+	{
+		h = h->next;
+		elem++;
+	}
+	return (elem);
+}
