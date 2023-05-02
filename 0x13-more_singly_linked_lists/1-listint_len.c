55197200b3ac85e38e770de0c09af85ce1a40717
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-05-02 20:02:55 +0300

    Betty style coding correction

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,11 +7,11 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t elem = 0;
-	while (h)
-	{
-		h = h->next;
-		elem++;
-	}
-	return (elem);
+size_t elem = 0;
+while (h)
+{
+h = h->next;
+elem++;
+}
+return (elem);
 }
