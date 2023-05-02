f1ff443e36ddf27f3c6b44d9254a43e8c6db843a
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-05-02 20:37:36 +0300

    Betty style code correction for blank line

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,24 +8,24 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *temp, *last_node;
-	last_node = malloc(sizeof(listint_t));
-	if (last_node == NULL)
-	return (NULL);
-	last_node->n = n;
-	last_node->next= NULL;
-	if (*head == NULL)
-	{
-	*head = last_node;
-	}
-	else
-	{
-	temp = *head;
-	while(temp->next != NULL)
-	{
-	temp = temp->next;
-	}
-	}
-	temp->next = last_node;
-	return (last_node);
+listint_t *temp, *last_node;
+last_node = malloc(sizeof(listint_t));
+if (last_node == NULL)
+return (NULL);
+last_node->n = n;
+last_node->next = NULL;
+if (*head == NULL)
+{
+*head = last_node;
+}
+else
+{
+temp = *head;
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+}
+temp->next = last_node;
+return (last_node);
 }
