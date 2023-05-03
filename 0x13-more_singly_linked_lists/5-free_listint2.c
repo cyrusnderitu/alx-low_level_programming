ee2e3d61144d9eed6b91b53870f7949edc72bf78
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-05-02 18:25:18 -0700

    Free a double head node

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees listint_t list
+ * @*head: pointer to node to free
+ */
+void free_listint2(listint_t **head)
+{
+	while (*head)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+	*head = NULL;
+}
