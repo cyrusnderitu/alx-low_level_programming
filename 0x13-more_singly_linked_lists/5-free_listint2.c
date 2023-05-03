de1aecce32e91637b5ec8e96a3c9baac552cc777
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-05-03 04:32:58 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t = *temp;
 	if (head == NULL)
-	return (NULL);
+		return;
 	while (*head)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = temp;
 	}
 	head = NULL;
 }
