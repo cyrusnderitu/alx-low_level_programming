7a002dbe109634b9ddeb25e966d3b8c2615f907f
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-05-05 04:28:19 +0300

    Betty style blank line missing correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len = 0;
-	if (*b == '\0')
+unsigned int num = 0, mult = 1;
+int len = 0;
+if (*b == '\0')
+return (0);
+while (b[len])
+len++;
+for (len -= 1; len >= 0; len--)
+{
+	if (b[len] != '0' && b[len] != '1')
 	return (0);
-	while (b[len])
-	len++;
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
-	}
-
-	return (num);
+	num += (b[len] - '0') * mult;
+	mult *= 2;
+}
+return (num);
 }
