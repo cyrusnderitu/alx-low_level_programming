cfff73eed73530718a20c7e0d68de50d6937bca9
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-05-04 18:26:29 -0700

    Binary conversionto number function

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts binary to number
+ * @b: pointer to string of 0 and 1's
+ * Return: covnerted num of b else (0)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len = 0;
+	if (*b == '\0')
+	return (0);
+	while (b[len])
+	len++;
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,13 @@
+# 0x14. C - Bit manipulation
+
+## Requirements
+
+### General
+
+- Allowed editors: **vi**, **vim**, **emacs**
+- All your files will be compiled on Ubuntu 20.04 LTS using gcc
+- All your files should end with a new line
+- A **README.md** file, at the root of the folder of the project is mandatory
+- Code should use the Betty style. It will be checked using **betty-style.pl** and **betty-doc.pl**
+- Global variable node allowed
+- Use of **_putchar** allowed
