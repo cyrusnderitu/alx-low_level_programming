8ddbc219418c025389355b8902a297149003e316
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-07-06 07:15:22 -0700

    Converter from binary to decimal && git push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts binary to int
- * @b: String of 0 and 1 char
- * Return: the converted number
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: Character pointing to a string of numbers.
+ * Return:  Converted number, else 0 if
+ *     b is null or b has a non integer value
  */
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int sum = 0;
-if (b == NULL)
-	return 0;
-int i = 0;
-while (b[i] != '\0')
+if (*b == '\0')
+	return (0);
+while (*b != '\0')
 {
-	if (b[i] != '0' && b[i] != '1')
-		return 0;
-	sum = (sum << 1) + (b[i] - '0');
-i++;
+    if (*b != '0' && *b != '1')
+        return 0;
+    sum = (*b - '0') + sum * 2;
+    b++;
 }
 return sum;
 }
