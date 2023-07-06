09d14b7e183f3155f619da17f0728bbbf023470b
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-06 17:19:16 +0300

    Betty style correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,20 +3,21 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: Character pointing to a string of numbers.
- * Return:  Converted number, else 0 if
- *     b is null or b has a non integer value
+ * Return:  Converted number, else 0.
  */
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int sum = 0;
 if (*b == '\0')
-	return (0);
+{
+return (0);
+}
 while (*b != '\0')
 {
-    if (*b != '0' && *b != '1')
-        return 0;
-    sum = (*b - '0') + sum * 2;
-    b++;
+if (*b != '0' && *b != '1')
+return 0;
+sum = (*b - '0') + sum * 2;
+b++;
 }
 return sum;
 }
