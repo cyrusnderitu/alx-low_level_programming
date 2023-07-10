7479258231e4e2dbf2eff44a6691a78af4999c8e
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-07-10 07:26:46 -0700

    Reading text from a file and printing to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and
+ *      prints it to the POSIX standard output.
+ * @filename: Pointer to the file descriptor.
+ * @letters: length of the fd.
+ * Return: Actual number of letters.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, rd, wr;
+	char *buffer;
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	rd = read(fd, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (fd == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (wr);
+}
+
