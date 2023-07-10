c7c01a107dce1930f820ad90e6e22afe9ff956f0
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-10 17:36:24 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,27 +9,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, rd, wr;
-	char *buffer;
-	if (filename == NULL)
-	{
-		return (0);
-	}
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		return (0);
-	}
-	fd = open(filename, O_RDONLY);
-	rd = read(fd, buffer, letters);
-	wr = write(STDOUT_FILENO, buffer, rd);
-	if (fd == -1 || rd == -1 || wr == -1 || wr != rd)
-	{
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
-	close(fd);
-	return (wr);
+ssize_t fd, rd, wr;
+char *buffer;
+if (filename == NULL)
+{
+return (0);
 }
-
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+return (0);
+}
+fd = open(filename, O_RDONLY);
+rd = read(fd, buffer, letters);
+wr = write(STDOUT_FILENO, buffer, rd);
+if (fd == -1 || rd == -1 || wr == -1 || wr != rd)
+{
+free(buffer);
+return (0);
+}
+free(buffer);
+close(fd);
+return (wr);
+}
