3b8de44696717d6aed9aecada0554b18b009f5d3
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-11 16:15:06 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 int fd, wr, len;
 if (filename == NULL)
 {
-return (-1);
+return (1);
 }
 fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 if (text_content != NULL)
